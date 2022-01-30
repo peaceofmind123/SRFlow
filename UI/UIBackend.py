d4446c4cef69d1b5a09428d0ea5ee9cc96f1730e
ashishthesatan <ashishthesatan@gmail.com>
2022-01-30 10:40:19 +0545

    refactored to implement uploadGT as well

--- UI/UIBackend.py
+++ UI/UIBackend.py
@@ -31,27 +31,33 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/upload/lr")
-async def uploadLR(file: UploadFile):
+async def uploadGeneral(file: UploadFile, type:str):
+    # generic function that works for both gt and lr
+    # arg: type: either 'gt' or 'lr'
     # get file extension
     ext = file.filename.split('.')[-1]
 
     # compute id
     num_uploads = session.query(Upload).count()
-    id = num_uploads # the id of the new upload will be the length of num_uploads
+    id = num_uploads  # the id of the new upload will be the length of num_uploads
 
     # create the upload object and add it into the table
     new_upload = Upload()
-    out_file_path=os.path.join(os.getcwd(), 'static','uploads','lr', str(id)+'.'+ext)
-    new_upload.url = '/static/uploads/lr/'+str(id)+'.'+ext
+    out_file_path = os.path.join(os.getcwd(), 'static', 'uploads', type, str(id) + '.' + ext)
+    new_upload.url = '/static/uploads/'+type+'/' + str(id) + '.' + ext
     session.add(new_upload)
 
     async with aiofiles.open(out_file_path, 'wb') as out_file:
         content = await file.read()  # async read
         await out_file.write(content)  # async write
     session.commit()
+
     return {"id": id, "url": new_upload.url}
 
+@app.post("/upload/lr")
+async def uploadLR(file: UploadFile):
+    return await uploadGeneral(file,'lr')
+
 @app.post("/upload/gt")
-async def uploadGT():
-    return {"status": "success"}
+async def uploadGT(file: UploadFile):
+    return await uploadGeneral(file,'gt')
